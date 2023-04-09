98f6c9f50370376b14293cf8e488f24b7b29a1a0
MitchellWoodUK <mitchell_wood@btinternet.com>
2023-04-09 15:38:38 +0100

    Added Family and FamilyMembers models. Then added some functionality into the settings page. Now need to create the controller pages.

---
+++ DissertationProject/Models/FamilyMembersModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DissertationProject.Models
+{
+    public class FamilyMembersModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public FamilyModel Family { get; set; }
+        [Required]
+        public CustomUserModel FamilyMember { get; set; }
+    }
+}
+
---
+++ DissertationProject/Models/FamilyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DissertationProject.Models
+{
+    public class FamilyModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
+
