2caec427a1d5d4d1988afec8463527eb63c0efac
MitchellWoodUK <mitchell_wood@btinternet.com>
2023-05-02 21:36:12 +0100

    Created a budget model and view model

---
+++ DissertationProject/Models/BudgetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DissertationProject.Models
+{
+    public class BudgetViewModel
+    {
+        public FamilyBudgetModel Budget { get; set; }
+
+        public float Profit { get; set; }
+
+        public float Expenses { get; set; }
+    }
+}
+
---
+++ DissertationProject/Models/FamilyBudgetModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DissertationProject.Models
+{
+    public class FamilyBudgetModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Family")]
+        public int FamilyId { get; set; }
+        public FamilyModel Family { get; set; }
+
+        public float Budget { get; set; }
+    }
+}
+
